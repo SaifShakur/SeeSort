9f6c53bc955a6eddc10ea536457d985bb42dc2eb
SaifShakur <saifmshakur@gmail.com>
2018-08-30 00:15:06 -0400

    test

---
+++ example.js
@@ -0,0 +1,24 @@
+
+createTimer = function(){
+
+
+}
+
+$(document).ready(function(){
+
+	$("#Random").click(function(){
+		alert("This works so far...");
+	});
+
+	//array_button.textContent = "Hide the array"
+
+	
+
+});
+
+
+/*
+	a timer is set to "go" when the "Go" button is pressed
+*/
+
+
