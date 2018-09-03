ef0c60bdc953e7a8a936b66e07087d8084525550
SaifShakur <saifmshakur@gmail.com>
2018-09-03 16:09:13 -0400

    09032018

--- example.js
+++ example.js
@@ -1,34 +1,71 @@
 
-createTimer = function(){
+// createTimer = function(){
+// };
 
+var user_array = [2, 4, 6, 8];
+var display = false;
 
-}
+var type_of_array = null;
+var order_of_array = null;
+var array_size = 0;
 
 $(document).ready(function(){
 
-	bool array_btn_switch = false;
+
+	$("#the_array").text(user_array);
+
 
 	$("#Random").click(function(){
-		alert("This works so far...");
+		type_of_array = "Random";
+		console.log(type_of_array);
+	});
+	$("#Sorted").click(function(){
+		type_of_array = "Sorted";
+		console.log(type_of_array);
 	});
 
+
+	$("#Ascending").click(function(){
+		order_of_array = "Ascending";
+		console.log(order_of_array);
+	});
+	$("#Descending").click(function(){
+		order_of_array = "Descending";
+		console.log(order_of_array);
+	});
+
+
+	//used to toggle between displaying/hiding the array
 	$("#array_button").click(function(){
 
-		
-		// if($(this).textContent == "Hide the array")
-		// 	$(this).textContent = "Show the array";
-		// else
-		// 	$(this).textContent = "Hide the array";	
+		console.log(display);
+		console.log(array_size);
+
+		if(display === false){
+			$("#the_array").show();
+			$(this).text("Hide the array");
+			display = true; 
+		}
+		else{
+			$("#the_array").hide();
+			$(this).text("Show the array");
+			display = false;
+		}
+
 	});
 
-	//array_button.textContent = "Hide the array"
-	//test comment
-	//test
-	//test 08/31/2018
 
-	
+	// //used for the user's submission of the size of the array
+	$("#size_button").click(function(){
+		array_size = $("#size_of_array").val();
+		console.log(array_size);
+		$("#size_of_array").val("");
+	});
+
+
 
 });
+
 
 
 /*
