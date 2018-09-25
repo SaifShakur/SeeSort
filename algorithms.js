d4b3b331831c056c7983a01bdc386ec6e42a7457
SaifShakur <saifmshakur@gmail.com>
2018-09-25 19:28:29 -0400

    need to adjust merge

--- algorithms.js
+++ algorithms.js
@@ -1,5 +1,8 @@
 //created: 09/03/2018
 
+/*
+||||||||||||||||||||||----Insertion Sort----|||||||||||||||||||||
+*/
 //Insertion sort from Hackernoon.com: https://hackernoon.com/programming-with-js-insertion-sort-1316df8354f5
 insertion_sort = function(user_array, order_of_array){
 
@@ -31,6 +34,9 @@ insertion_sort = function(user_array, order_of_array){
 
 }
 
+/*
+||||||||||||||||||||||----Bubble Sort----|||||||||||||||||||||
+*/
 bubble_sort = function(user_array, order_of_array){
 
 	if(order_of_array === "Ascending"){
@@ -63,10 +69,62 @@ bubble_sort = function(user_array, order_of_array){
 
 }
 
+/*
+||||||||||||||||||||||----Merge Sort----|||||||||||||||||||||
+*/
+//Merge sort functions from Hackernoon.com: https://hackernoon.com/programming-with-js-merge-sort-deb677b777c0
 merge_sort = function(user_array, order_of_array){
 
+	if (user_array.length === 1) 			//return once we hit an array with a single item
+    	return user_array;
+
+	const middle = Math.floor(user_array.length / 2); // get the middle item of the array rounded down
+	const left = user_array.slice(0, middle); // items on the left side
+	const right = user_array.slice(middle); // items on the right side
+
 	
+	return merge(merge_sort(left, order_of_array), merge_sort(right, order_of_array), order_of_array)
+
 }
+// compare the arrays item by item and return the concatenated result
+merge = function(left, right, order_of_array) {
+  let result = []
+  let indexLeft = 0
+  let indexRight = 0
+
+  while (indexLeft < left.length && indexRight < right.length) {
+
+  	if(order_of_array === "Ascending"){
+	    if (left[indexLeft] < right[indexRight]) {
+	      result.push(left[indexLeft])
+	      indexLeft++
+	    } else {
+	      result.push(right[indexRight])
+	      indexRight++
+	    }
+	}
+	else if(order_of_array === "Descending"){
+		if (left[indexLeft] > right[indexRight]) {
+	      result.push(left[indexLeft])
+	      indexLeft++
+	    } else {
+	      result.push(right[indexRight])
+	      indexRight++
+	    }
+	}
+  }
+
+  return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight))
+}
+
+
+
+
+
+
+
+	
+
 
 
 
